19ff37e19a0c2e19f764105d96c6656f1fc9becd
danieltruong <aduy1122@gmail.com>
2022-06-03 17:44:47 -0700

    Update api link

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/v1", (req, resp) => {
         const image = $(this).find("a > img").attr("data-src");
         thumbnails.push({
           name: name,
-          url: "http://localhost:8000/v1" + url.split("/wiki")[1],
+          url: "https://demon-slayer-api.onrender.com/v1" + url.split("/wiki")[1],
           image: image,
         });
       });
